d0e92cd339bd4a0fd2c8b4427a610a8e91d20b40
wizzycode <dicksonwisdom005@gmail.com>
2025-10-05 11:10:33 +0100

    added files

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar"
 import Hero from "../components/Horo"
 import Work from "../components/Work"
 import Project from "../components/Projects"
-import Contributions from "../components/contributions"
+import Contributions from "../components/Contributions"
 import Blog from "../components/Blog"
 import Footer from "../components/Footer"
 
